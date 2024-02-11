77ae746e4a10ca92120717fee54905b695aeaae1
M1ker1n <35259381+m1ker1n@users.noreply.github.com>
2024-02-11 15:50:13 +0300

    done hw3 (pprof) (#2)
    * got pprof data for report
    
    * moved body of SlowSearch into FastSearch
    
    * optimised regexp usage
    
    * fixed MD for readability
    
    * optimized unmarshalling, now it fills struct not map[string]any
    
    * little refactor
    
    * changed json.Unmarshal to easyjson.Unmarshal
    
    * tested if reading by line is faster than reading all
    
    * fixed benchmarks ReadAll vs ReadByLine
    
    * optimized reading of data from file by reading by lines & without string conversions
    
    * optimized B/op by removing users accumulation
    
    * optimized B/op by removing users accumulation
    
    * optimized ns/op by removing unneeded cycle & using map instead of slice
    
    * optimized B/op & allocs/op by changing regexp replacement with strings replacement
    
    * moved img*.png to images/

--- 3/99_hw/fast.go
+++ 3/99_hw/fast.go
@@ -1,18 +1,66 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"github.com/mailru/easyjson"
+	"hw3/user"
 	"io"
+	"os"
+	"strings"
 )
 
-// вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	/*
-		!!! !!! !!!
-		обратите внимание - в задании обязательно нужен отчет
-		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
-		так же обратите внимание на команду в параметром -http
-		перечитайте еще раз задание
-		!!! !!! !!!
-	*/
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	seenBrowsers := map[string]bool{}
+	var foundUsersBuilder strings.Builder
+
+	for i := 0; scanner.Scan(); i++ {
+		var u user.User
+		// fmt.Printf("%v %v\n", err, line)
+		err := easyjson.Unmarshal(scanner.Bytes(), &u)
+		if err != nil {
+			panic(err)
+		}
+
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range u.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				if _, seenBefore := seenBrowsers[browser]; !seenBefore {
+					// log.Printf("FAST New browser: %s, first seen: %s", browser, u.Name)
+					seenBrowsers[browser] = true
+				}
+			}
+
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				if _, seenBefore := seenBrowsers[browser]; !seenBefore {
+					// log.Printf("FAST New browser: %s, first seen: %s", browser, u.Name)
+					seenBrowsers[browser] = true
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", u.Name, u.Email)
+		email := strings.ReplaceAll(u.Email, "@", " [at] ")
+		foundUsersBuilder.WriteString(fmt.Sprintf("[%d] %s <%s>\n", i, u.Name, email))
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsersBuilder.String())
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
--- 3/99_hw/main_test.go
+++ 3/99_hw/main_test.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/json"
+	"github.com/mailru/easyjson"
+	"hw3/user"
 	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -42,5 +49,121 @@ func BenchmarkSlow(b *testing.B) {
 func BenchmarkFast(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		FastSearch(ioutil.Discard)
+	}
+}
+
+const (
+	pattern = "Android"
+	browser = "123Android5363"
+)
+
+func BenchmarkRegexpMatchString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = regexp.MatchString(pattern, browser)
+	}
+}
+
+func BenchmarkPrecompiledRegexpMatchString(b *testing.B) {
+	r := regexp.MustCompile(pattern)
+	for i := 0; i < b.N; i++ {
+		r.MatchString(browser)
+	}
+}
+
+func BenchmarkStringsContains(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.Contains(browser, pattern)
+	}
+}
+
+const unmarshalData = `{"browsers":["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.32 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.32 Epiphany/3.8.2","Wget/1.9 cvs-stable (Red Hat modified)","Mozilla/3.0 (compatible; NetPositive/2.1.1; BeOS)","Mozilla/5.0 (X11; Linux 3.8-6.dmz.1-liquorix-686) KHTML/4.8.4 (like Gecko) Konqueror/4.8"],"company":"Meevee","country":"Fiji","email":"CarolynReyes@Mydeo.name","job":"Librarian","name":"Steven Burton","phone":"420-22-74"}`
+
+func BenchmarkUnmarshalToMap(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		u := make(map[string]interface{})
+		_ = json.Unmarshal([]byte(unmarshalData), &u)
+	}
+}
+
+func BenchmarkUnmarshalToStruct(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var u user.User
+		_ = json.Unmarshal([]byte(unmarshalData), &u)
+	}
+}
+
+func BenchmarkEasyjsonUnmarshal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var u user.User
+		_ = easyjson.Unmarshal([]byte(unmarshalData), &u)
+	}
+}
+
+func BenchmarkReadAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		file, err := os.Open(filePath)
+		if err != nil {
+			panic(err)
+		}
+		fileContents, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+
+		lines := strings.Split(string(fileContents), "\n")
+
+		users := make([]user.User, 0)
+		for _, line := range lines {
+			var u user.User
+			// fmt.Printf("%v %v\n", err, line)
+			err := easyjson.Unmarshal([]byte(line), &u)
+			if err != nil {
+				panic(err)
+			}
+			users = append(users, u)
+		}
+	}
+}
+
+func BenchmarkReadByLine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		file, err := os.Open(filePath)
+		if err != nil {
+			panic(err)
+		}
+		scanner := bufio.NewScanner(file)
+
+		users := make([]user.User, 0)
+		for scanner.Scan() {
+			var u user.User
+			// fmt.Printf("%v %v\n", err, line)
+			err := easyjson.Unmarshal(scanner.Bytes(), &u)
+			if err != nil {
+				panic(err)
+			}
+			users = append(users, u)
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+const (
+	email       = "m1ker1nigor@yandex.ru"
+	replaceable = "@"
+	replacement = " [at] "
+)
+
+func BenchmarkReplaceByRegexp(b *testing.B) {
+	r := regexp.MustCompile(replaceable)
+	for i := 0; i < b.N; i++ {
+		r.ReplaceAllString(email, replacement)
+	}
+}
+
+func BenchmarkReplaceByStrings(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.ReplaceAll(email, replaceable, replacement)
 	}
 }
