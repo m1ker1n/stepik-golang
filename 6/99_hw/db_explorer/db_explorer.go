a8cef4dbbb6e86923e75db3da01680176525d569
m1ker1n <m1ker1nigor@yandex.ru>
2024-02-24 21:46:11 +0300

    implemented DBExplorer

--- 6/99_hw/db_explorer/db_explorer.go
+++ 6/99_hw/db_explorer/db_explorer.go
@@ -9,18 +9,28 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/m1ker1n/go-generics"
 	"net/http"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 type HttpResponse struct {
-	Error    error `json:"error,omitempty"`
-	Response any   `json:"response,omitempty"`
+	Error    string `json:"error,omitempty"`
+	Response any    `json:"response,omitempty"`
 }
 
 func (r HttpResponse) Write(w http.ResponseWriter, status int) {
 	result, _ := json.Marshal(r)
 	w.WriteHeader(status)
 	w.Write(result)
+}
+
+func NewErrorResponse(err error) HttpResponse {
+	return HttpResponse{Error: err.Error()}
+}
+
+func NewResponse(v any) HttpResponse {
+	return HttpResponse{Response: v}
 }
 
 type Column struct {
@@ -40,6 +50,17 @@ type MySQLDBExplorerService struct {
 
 	tablesColumns map[string][]Column
 }
+
+type ErrInvalidFieldType string
+
+func (e ErrInvalidFieldType) Error() string {
+	return fmt.Sprintf("field %s have invalid type", string(e))
+}
+
+var (
+	ErrTableNotFound  = errors.New("unknown table")
+	ErrRecordNotFound = errors.New("record not found")
+)
 
 func NewMySQLDBExplorerService(db *sql.DB) (*MySQLDBExplorerService, error) {
 	tables, err := getTables(db)
@@ -126,10 +147,16 @@ func getTableColumns(db *sql.DB, table string) ([]Column, error) {
 	return columns, nil
 }
 
+func (s *MySQLDBExplorerService) GetTables() []string {
+	tables := generics.MapKeys(s.tablesColumns)
+	sort.Strings(tables)
+	return tables
+}
+
 func (s *MySQLDBExplorerService) GetTableRows(ctx context.Context, table string, limit, offset int) ([]map[string]any, error) {
 	columns, tableExists := s.tablesColumns[table]
 	if !tableExists {
-		return nil, fmt.Errorf("table %s doesn't exists", table)
+		return nil, ErrTableNotFound
 	}
 
 	results := make([]map[string]any, 0, limit)
@@ -190,7 +217,7 @@ func (s *MySQLDBExplorerService) GetTableRows(ctx context.Context, table string,
 func (s *MySQLDBExplorerService) GetTableRow(ctx context.Context, table string, key any) (map[string]any, error) {
 	columns, tableExists := s.tablesColumns[table]
 	if !tableExists {
-		return nil, fmt.Errorf("table %s doesn't exists", table)
+		return nil, ErrTableNotFound
 	}
 
 	primaryKeyColumn, ok := generics.FindFirst(columns, func(x Column) bool {
@@ -212,6 +239,10 @@ func (s *MySQLDBExplorerService) GetTableRow(ctx context.Context, table string, 
 	}
 
 	if err := row.Scan(scanInto...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrRecordNotFound
+		}
+
 		return nil, err
 	}
 
@@ -241,18 +272,13 @@ func (s *MySQLDBExplorerService) GetTableRow(ctx context.Context, table string, 
 func (s *MySQLDBExplorerService) CreateTableRow(ctx context.Context, table string, data map[string]any) (map[string]any, error) {
 	columns, tableExists := s.tablesColumns[table]
 	if !tableExists {
-		return nil, fmt.Errorf("table %s doesn't exists", table)
+		return nil, ErrTableNotFound
 	}
 
-	dataFields := generics.MapKeys(data)
-	allowedFields := generics.Filter(dataFields, func(x string) bool {
-		for _, column := range columns {
-			if column.Field == x && column.Extra != "auto_increment" {
-				return true
-			}
-		}
-		return false
-	})
+	allowedFields, err := filterFieldsForCreate(columns, data)
+	if err != nil {
+		return nil, err
+	}
 
 	colNames := fmt.Sprintf("`%s`", strings.Join(allowedFields, "`, `"))
 	placeholders := strings.Join(strings.Split(strings.Repeat("?", len(allowedFields)), ""), ", ")
@@ -272,33 +298,35 @@ func (s *MySQLDBExplorerService) CreateTableRow(ctx context.Context, table strin
 		return nil, err
 	}
 
-	result := make(map[string]any)
-	result["updated"] = insertedId
-
-	return result, nil
-}
-
-func (s *MySQLDBExplorerService) UpdateTableRow(ctx context.Context, table string, key any, data map[string]any) (map[string]any, error) {
-	columns, tableExists := s.tablesColumns[table]
-	if !tableExists {
-		return nil, fmt.Errorf("table %s doesn't exists", table)
-	}
-
 	primaryKeyColumn, ok := generics.FindFirst(columns, func(x Column) bool {
 		return x.Key == "PRI"
 	})
 	if !ok {
 		return nil, errors.New("primary key column is not found")
 	}
-	dataFields := generics.MapKeys(data)
-	allowedFields := generics.Filter(dataFields, func(x string) bool {
-		for _, column := range columns {
-			if column.Field == x && column.Extra != "auto_increment" {
-				return true
-			}
-		}
-		return false
+
+	result := map[string]any{
+		primaryKeyColumn.Field: insertedId,
+	}
+	return result, nil
+}
+
+func (s *MySQLDBExplorerService) UpdateTableRow(ctx context.Context, table string, key any, data map[string]any) (int64, error) {
+	columns, tableExists := s.tablesColumns[table]
+	if !tableExists {
+		return 0, ErrTableNotFound
+	}
+
+	primaryKeyColumn, ok := generics.FindFirst(columns, func(x Column) bool {
+		return x.Key == "PRI"
 	})
+	if !ok {
+		return 0, errors.New("primary key column is not found")
+	}
+	allowedFields, err := filterFieldsForUpdate(columns, data)
+	if err != nil {
+		return 0, err
+	}
 
 	assignments := generics.Map(allowedFields, func(field string) (string, error) {
 		return fmt.Sprintf("`%s`=?", field), nil
@@ -314,31 +342,24 @@ func (s *MySQLDBExplorerService) UpdateTableRow(ctx context.Context, table strin
 	args[len(allowedFields)] = key
 	sqlResult, err := s.db.ExecContext(ctx, query, args...)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
 	affectedRows, err := sqlResult.RowsAffected()
-	if err != nil {
-		return nil, err
-	}
-
-	result := make(map[string]any)
-	result["updated"] = affectedRows
-
-	return result, nil
+	return affectedRows, err
 }
 
-func (s *MySQLDBExplorerService) DeleteTableRow(ctx context.Context, table string, key any) (map[string]any, error) {
+func (s *MySQLDBExplorerService) DeleteTableRow(ctx context.Context, table string, key any) (int64, error) {
 	columns, tableExists := s.tablesColumns[table]
 	if !tableExists {
-		return nil, fmt.Errorf("table %s doesn't exists", table)
+		return 0, ErrTableNotFound
 	}
 
 	primaryKeyColumn, ok := generics.FindFirst(columns, func(x Column) bool {
 		return x.Key == "PRI"
 	})
 	if !ok {
-		return nil, errors.New("couldn't find primary key field")
+		return 0, errors.New("couldn't find primary key field")
 	}
 	// At the moment we checked if table is valid with looking up in s.tablesColumns[table].
 	// So there can't be SQL-injection in the table variable.
@@ -346,26 +367,113 @@ func (s *MySQLDBExplorerService) DeleteTableRow(ctx context.Context, table strin
 	query := fmt.Sprintf("DELETE FROM %s WHERE %s = ?", table, primaryKeyColumn.Field)
 	sqlResult, err := s.db.ExecContext(ctx, query, key)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
 	rowsDeleted, err := sqlResult.RowsAffected()
-	if err != nil {
-		return nil, err
+	return rowsDeleted, err
+}
+
+func filterFieldsForUpdate(columns []Column, data map[string]any) ([]string, error) {
+	allowedFields := make([]string, 0, len(data))
+DataLoop:
+	for dataKey, dataValue := range data {
+		for _, column := range columns {
+			if dataKey == column.Field {
+				// column is found
+				if column.Key == "PRI" || column.Extra == "auto_increment" {
+					// can't change primary keys & fields with auto increment
+					return nil, ErrInvalidFieldType(dataKey)
+				}
+
+				switch dataValue.(type) {
+				case string:
+					switch column.Type {
+					case "varchar(255)", "text":
+					default:
+						return nil, ErrInvalidFieldType(dataKey)
+					}
+				case int:
+					switch column.Type {
+					case "int":
+					default:
+						return nil, ErrInvalidFieldType(dataKey)
+					}
+				case float64:
+					switch column.Type {
+					case "float":
+					default:
+						return nil, ErrInvalidFieldType(dataKey)
+					}
+				}
+
+				if dataValue == nil && column.Null == "NO" {
+					return nil, ErrInvalidFieldType(dataKey)
+				}
+
+				//it's good field
+				allowedFields = append(allowedFields, dataKey)
+				continue DataLoop
+			}
+		}
+		// column is not found => skip it
 	}
+	return allowedFields, nil
+}
 
-	result := make(map[string]any)
-	result["deleted"] = rowsDeleted
+func filterFieldsForCreate(columns []Column, data map[string]any) ([]string, error) {
+	allowedFields := make([]string, 0, len(data))
+DataLoop:
+	for dataKey, dataValue := range data {
+		for _, column := range columns {
+			if dataKey == column.Field {
+				// column is found
+				if column.Key == "PRI" || column.Extra == "auto_increment" {
+					continue DataLoop
+				}
 
-	return result, nil
+				switch dataValue.(type) {
+				case string:
+					switch column.Type {
+					case "varchar(255)", "text":
+					default:
+						return nil, ErrInvalidFieldType(dataKey)
+					}
+				case int:
+					switch column.Type {
+					case "int":
+					default:
+						return nil, ErrInvalidFieldType(dataKey)
+					}
+				case float64:
+					switch column.Type {
+					case "float":
+					default:
+						return nil, ErrInvalidFieldType(dataKey)
+					}
+				}
+
+				if dataValue == nil && column.Null == "NO" {
+					return nil, ErrInvalidFieldType(dataKey)
+				}
+
+				//it's good field
+				allowedFields = append(allowedFields, dataKey)
+				continue DataLoop
+			}
+		}
+		// column is not found => skip it
+	}
+	return allowedFields, nil
 }
 
 type DBExplorerService interface {
+	GetTables() []string
 	GetTableRows(ctx context.Context, table string, limit, offset int) ([]map[string]any, error)
 	GetTableRow(ctx context.Context, table string, key any) (map[string]any, error)
 	CreateTableRow(ctx context.Context, table string, data map[string]any) (map[string]any, error)
-	UpdateTableRow(ctx context.Context, table string, key any, data map[string]any) (map[string]any, error)
-	DeleteTableRow(ctx context.Context, table string, key any) (map[string]any, error)
+	UpdateTableRow(ctx context.Context, table string, key any, data map[string]any) (int64, error)
+	DeleteTableRow(ctx context.Context, table string, key any) (int64, error)
 }
 
 type DBExplorer struct {
@@ -383,64 +491,214 @@ func NewDbExplorer(db *sql.DB) (*DBExplorer, error) {
 	}, nil
 }
 
-var (
-	ErrMethodNotFound = errors.New("method for this url is not found")
-)
-
 func (srv *DBExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		switch r.Method {
-		case http.MethodGet:
-			srv.GetAllTables(w, r)
-		default:
-			HttpResponse{Error: ErrMethodNotFound}.Write(w, http.StatusMethodNotAllowed)
-		}
-	case "/{table}":
-		switch r.Method {
-		case http.MethodGet:
-			srv.GetTableRows(w, r)
-		case http.MethodPost:
-			srv.PostTableRow(w, r)
-		default:
-			HttpResponse{Error: ErrMethodNotFound}.Write(w, http.StatusMethodNotAllowed)
-		}
-	case "/{table}/{id}":
-		switch r.Method {
-		case http.MethodGet:
-			srv.GetTableRow(w, r)
-		case http.MethodPut:
-			srv.PutTableRow(w, r)
-		case http.MethodDelete:
-			srv.DeleteTableRow(w, r)
-		default:
-			HttpResponse{Error: ErrMethodNotFound}.Write(w, http.StatusMethodNotAllowed)
-		}
-	default:
-		http.NotFound(w, r)
-	}
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", srv.GetAllTables)
+	mux.HandleFunc("GET /{table}", srv.GetTableRows)
+	mux.HandleFunc("PUT /{table}/", srv.PutTableRow)
+	mux.HandleFunc("GET /{table}/{id}", srv.GetTableRow)
+	mux.HandleFunc("POST /{table}/{id}", srv.PostTableRow)
+	mux.HandleFunc("DELETE /{table}/{id}", srv.DeleteTableRow)
+
+	mux.ServeHTTP(w, r)
 }
 
-func (srv *DBExplorer) GetAllTables(w http.ResponseWriter, r *http.Request) {
-
+func (srv *DBExplorer) GetAllTables(w http.ResponseWriter, _ *http.Request) {
+	NewResponse(map[string]any{
+		"tables": srv.service.GetTables(),
+	}).Write(w, http.StatusOK)
 }
 
 func (srv *DBExplorer) GetTableRows(w http.ResponseWriter, r *http.Request) {
+	table := r.PathValue("table")
+	if table == "" {
+		NewErrorResponse(ErrTableNotFound).Write(w, http.StatusNotFound)
+		return
+	}
 
+	err := r.ParseForm()
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+	limitRaw, offsetRaw := r.Form.Get("limit"), r.Form.Get("offset")
+	limit, err := strconv.Atoi(limitRaw)
+	if err != nil {
+		limit = 5
+	}
+	offset, err := strconv.Atoi(offsetRaw)
+	if err != nil {
+		offset = 0
+	}
+
+	rows, err := srv.service.GetTableRows(r.Context(), table, limit, offset)
+	if err != nil {
+		if errors.Is(err, ErrTableNotFound) {
+			NewErrorResponse(err).Write(w, http.StatusNotFound)
+			return
+		}
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	NewResponse(map[string]any{
+		"records": rows,
+	}).Write(w, http.StatusOK)
 }
 
 func (srv *DBExplorer) GetTableRow(w http.ResponseWriter, r *http.Request) {
+	table := r.PathValue("table")
+	if table == "" {
+		NewErrorResponse(ErrTableNotFound).Write(w, http.StatusNotFound)
+		return
+	}
 
-}
+	id := r.PathValue("id")
+	if id == "" {
+		NewErrorResponse(errors.New("unknown id")).Write(w, http.StatusNotFound)
+		return
+	}
 
-func (srv *DBExplorer) PostTableRow(w http.ResponseWriter, r *http.Request) {
+	row, err := srv.service.GetTableRow(r.Context(), table, id)
+	if err != nil {
+		if errors.Is(err, ErrTableNotFound) {
+			NewErrorResponse(err).Write(w, http.StatusNotFound)
+			return
+		}
+		if errors.Is(err, ErrRecordNotFound) {
+			NewErrorResponse(err).Write(w, http.StatusNotFound)
+			return
+		}
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
 
+	NewResponse(map[string]any{
+		"record": row,
+	}).Write(w, http.StatusOK)
 }
 
 func (srv *DBExplorer) PutTableRow(w http.ResponseWriter, r *http.Request) {
+	table := r.PathValue("table")
+	if table == "" {
+		NewErrorResponse(ErrTableNotFound).Write(w, http.StatusNotFound)
+		return
+	}
 
+	err := r.ParseForm()
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusBadRequest)
+		return
+	}
+
+	var body map[string]any
+	err = json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	result, err := srv.service.CreateTableRow(r.Context(), table, body)
+	if err != nil {
+		if errors.Is(err, ErrTableNotFound) {
+			NewErrorResponse(err).Write(w, http.StatusNotFound)
+			return
+		}
+		var invalidFieldErr ErrInvalidFieldType
+		if errors.As(err, &invalidFieldErr) {
+			NewErrorResponse(err).Write(w, http.StatusBadRequest)
+			return
+		}
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	NewResponse(result).Write(w, http.StatusOK)
+}
+
+func (srv *DBExplorer) PostTableRow(w http.ResponseWriter, r *http.Request) {
+	table := r.PathValue("table")
+	if table == "" {
+		NewErrorResponse(ErrTableNotFound).Write(w, http.StatusNotFound)
+		return
+	}
+
+	idRaw := r.PathValue("id")
+	if idRaw == "" {
+		NewErrorResponse(errors.New("unknown id")).Write(w, http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.Atoi(idRaw)
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusBadRequest)
+		return
+	}
+
+	err = r.ParseForm()
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusBadRequest)
+		return
+	}
+
+	var body map[string]any
+	err = json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	updatedRows, err := srv.service.UpdateTableRow(r.Context(), table, id, body)
+	if err != nil {
+		if errors.Is(err, ErrTableNotFound) {
+			NewErrorResponse(err).Write(w, http.StatusNotFound)
+			return
+		}
+
+		var invalidFieldErr ErrInvalidFieldType
+		if errors.As(err, &invalidFieldErr) {
+			NewErrorResponse(err).Write(w, http.StatusBadRequest)
+			return
+		}
+
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	NewResponse(map[string]any{
+		"updated": updatedRows,
+	}).Write(w, http.StatusOK)
 }
 
 func (srv *DBExplorer) DeleteTableRow(w http.ResponseWriter, r *http.Request) {
+	table := r.PathValue("table")
+	if table == "" {
+		NewErrorResponse(ErrTableNotFound).Write(w, http.StatusNotFound)
+		return
+	}
 
+	idRaw := r.PathValue("id")
+	if idRaw == "" {
+		NewErrorResponse(errors.New("unknown id")).Write(w, http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.Atoi(idRaw)
+	if err != nil {
+		NewErrorResponse(err).Write(w, http.StatusBadRequest)
+		return
+	}
+
+	deletedRows, err := srv.service.DeleteTableRow(r.Context(), table, id)
+	if err != nil {
+		if errors.Is(err, ErrTableNotFound) {
+			NewErrorResponse(err).Write(w, http.StatusNotFound)
+			return
+		}
+		NewErrorResponse(err).Write(w, http.StatusInternalServerError)
+		return
+	}
+
+	NewResponse(map[string]any{
+		"deleted": deletedRows,
+	}).Write(w, http.StatusOK)
 }
