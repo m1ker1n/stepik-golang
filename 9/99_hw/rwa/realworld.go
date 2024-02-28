82e795f099be552853452611c053be90d5667f05
m1ker1n <m1ker1nigor@yandex.ru>
2024-02-28 23:00:15 +0300

    created interfaces for handling http requests

--- 9/99_hw/rwa/realworld.go
+++ 9/99_hw/rwa/realworld.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-// сюда писать код
+type UserHandler interface {
+	Login(http.ResponseWriter, *http.Request)
+	Logout(http.ResponseWriter, *http.Request)
+	Register(http.ResponseWriter, *http.Request)
+	GetCurrentUser(http.ResponseWriter, *http.Request)
+	UpdateCurrentUser(http.ResponseWriter, *http.Request)
+}
+
+type ArticlesHandler interface {
+	// GetArticles responds with most recent articles globally.
+	// GetArticles can be filtered via query parameters.
+	GetArticles(http.ResponseWriter, *http.Request)
+
+	CreateArticle(http.ResponseWriter, *http.Request)
+}
 
 func GetApp() http.Handler {
-	return nil
+	r := mux.NewRouter()
+	var user UserHandler
+	r.HandleFunc("/users", user.Register).Methods("POST")
+	r.HandleFunc("/users/login", user.Login).Methods("POST")
+	r.HandleFunc("/users/login", user.Logout).Methods("POST")
+	r.HandleFunc("/user", user.GetCurrentUser).Methods("GET")
+	r.HandleFunc("/user", user.UpdateCurrentUser).Methods("PUT")
+
+	var articles ArticlesHandler
+	r.HandleFunc("/articles", articles.CreateArticle).Methods("POST")
+	r.HandleFunc("/articles", articles.GetArticles).Methods("GET")
+	return r
 }
